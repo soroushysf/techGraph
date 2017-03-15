598c48743494e71a7db0253818e0bfcf547081b1
soroush yousefi <soroush.ysf@gmail.com>
2017-03-15 15:28:24 +0330

    graph table fixed

--- front/app/components/GraphTable/graphTableController.js
+++ front/app/components/GraphTable/graphTableController.js
@@ -2,27 +2,19 @@
  * Created by soroush on 12/18/16.
  */
 
-app.controller('graphTableController', function ($scope) {
+app.controller('graphTableController', function ($scope, d3Node) {
 
     $scope.graphTableTitle = 'Graph Table';
     
     $scope.$on("graphTableData", function (event, data) {
-        var crNodes=[], i=0;
 
-        for(var key in data["nodeDp"]){
-            if(data["nodeDp"].hasOwnProperty(key)){
-                crNodes[i] = {
-                    'id' : data["nodeDp"][key][0]["@rid"],
-                    'title' : data["nodeDp"][key][0]["tech_title"],
-                    'type' : data["nodeDp"][key][0]["@type"],
-                    'version' : data["nodeDp"][key][0]["@version"],
-                    'class' : data["nodeDp"][key][0]["@class"]
-                }
-                i++;
-            }
-        }
+        var crNodes;
+
+        crNodes = d3Node.createNode(data["nodeDp"]);
+
+        console.log(crNodes);
         $scope.createdNodes = crNodes;
-    })
+    });
 
 
     $scope.currentPage = 1;
